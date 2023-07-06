10fb152b9799163aa4ce35cd8b12fab4bd825ae2
RoccoDev <hey@rocco.dev>
2023-07-06 05:46:35 +0200

    memory: fix unused warnings

--- src/helpers/memoryHelper.cpp
+++ src/helpers/memoryHelper.cpp
@@ -1,36 +1,46 @@
 #include "memoryHelper.h"
 
+// Need a function to act as the default value
+inline void catch_all() {
+  EXL_ABORT("Memory not yet loaded");
+}
+
+static void *(*Malloc)(size_t) = reinterpret_cast<void* (*)(size_t)>(catch_all);
+static void (*Free)(void*) = reinterpret_cast<void (*)(void*)>(catch_all);
+static void *(*Realloc)(void*, size_t) = reinterpret_cast<void* (*)(void*, size_t)>(catch_all);
+static void *(*AlignedAlloc)(size_t, size_t) = reinterpret_cast<void* (*)(size_t, size_t)>(catch_all);
+
 void Mem::Init() {
-  uintptr_t mallocFn = 0xDEADBEEF;
-  uintptr_t alignedAllocFn = 0xDEADBEEF;
-  uintptr_t reallocFn = 0xDEADBEEF;
-  uintptr_t freeFn = 0xDEADBEEF;
+  uintptr_t mallocFn;
+  uintptr_t alignedAllocFn;
+  uintptr_t reallocFn;
+  uintptr_t freeFn;
 
   R_ABORT_UNLESS(nn::ro::LookupSymbol(&mallocFn, "malloc"));
   R_ABORT_UNLESS(nn::ro::LookupSymbol(&alignedAllocFn, "aligned_alloc"));
   R_ABORT_UNLESS(nn::ro::LookupSymbol(&freeFn, "free"));
   R_ABORT_UNLESS(nn::ro::LookupSymbol(&reallocFn, "realloc"));
 
-  Mem::Malloc = reinterpret_cast<void *(*)(size_t)>(mallocFn);
-  Mem::AlignedAlloc = reinterpret_cast<void* (*)(size_t, size_t)>(alignedAllocFn);
-  Mem::Free = reinterpret_cast<void (*)(void*)>(freeFn);
-  Mem::Realloc = reinterpret_cast<void *(*)(void*, size_t)>(reallocFn);
+  Malloc = reinterpret_cast<void *(*)(size_t)>(mallocFn);
+  AlignedAlloc = reinterpret_cast<void* (*)(size_t, size_t)>(alignedAllocFn);
+  Free = reinterpret_cast<void (*)(void*)>(freeFn);
+  Realloc = reinterpret_cast<void *(*)(void*, size_t)>(reallocFn);
 
   Logger::log("Mem initialized\n");
 }
 
 void* Mem::Allocate(size_t size) {
-  return (Mem::Malloc)(size);
+  return (Malloc)(size);
 }
 
 void* Mem::AllocateAlign(size_t align, size_t size) {
-  return (Mem::AlignedAlloc)(align, size);
+  return (AlignedAlloc)(align, size);
 }
 
 void Mem::Deallocate(void *ptr) {
-  (Mem::Free)(ptr);
+  (Free)(ptr);
 }
 
 void* Mem::Reallocate(void *ptr, size_t new_size) {
-  return (Mem::Realloc)(ptr, new_size);
+  return (Realloc)(ptr, new_size);
 }
--- src/helpers/memoryHelper.h
+++ src/helpers/memoryHelper.h
@@ -10,17 +10,7 @@
 // Manual memory control: some games don't export or expose (with symbols) standard allocator calls.
 // Looks like C calls like malloc etc. are still present, though
 
-// Need a function to act as the default value
-inline void catch_all() {
-  EXL_ABORT("Memory not yet loaded");
-}
-
 namespace Mem {
-  static void *(*Malloc)(size_t) = reinterpret_cast<void* (*)(size_t)>(catch_all);
-  static void (*Free)(void*) = reinterpret_cast<void (*)(void*)>(catch_all);
-  static void *(*Realloc)(void*, size_t) = reinterpret_cast<void* (*)(void*, size_t)>(catch_all);
-  static void *(*AlignedAlloc)(size_t, size_t) = reinterpret_cast<void* (*)(size_t, size_t)>(catch_all);
-
   void Init();
 
   void* Allocate(size_t size);
